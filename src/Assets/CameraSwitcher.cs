9abc07c6cf57490bb64fc4a401b4eab45b10a6fd
Eric Freeman <clownhunter@gmail.com>
2017-11-21 18:36:17 -0700

    oops

---
+++ src/Assets/CameraSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class CameraSwitcher : MonoBehaviour
+    {
+        public Camera FirstPersonCamera;
+
+        public Camera ThirdPersonCamera;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftBracket))
+            {
+                FirstPersonCamera.enabled = true;
+                ThirdPersonCamera.enabled = false;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightBracket))
+            {
+                FirstPersonCamera.enabled = false;
+                ThirdPersonCamera.enabled = true;
+            }
+        }
+    }
+}
