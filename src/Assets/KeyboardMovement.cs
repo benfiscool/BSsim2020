525a8d6a59b2da5c939ba7baea35232073c6aa59
Ben Freeman <benfiscool@gmail.com>
2017-11-21 17:22:51 -0700

    initial commit
    initial commit

---
+++ src/Assets/KeyboardMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour {
+    private Animator _animator;
+    private float MoveSpeed = 1.5f;
+    
+	void Start () {
+        _animator = GetComponent<Animator>();
+	}
+    
+    void Update() {
+        var forward = Input.GetAxisRaw("Vertical");
+        var sideways = Input.GetAxisRaw("Horizontal");
+
+        _animator.SetFloat("Vertical", forward);
+        _animator.SetFloat("Horizontal", sideways);
+
+        if (forward != 0 || sideways != 0)
+        {
+            transform.Translate(sideways * MoveSpeed * Time.deltaTime, 0, forward * MoveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.enabled = !_animator.enabled;
+        }
+	}
+}
---
+++ src/Assets/MouseTurning.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MouseTurning : MonoBehaviour {
+    public float MouseSensetivity = 10f;
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update () {
+        var horizontalTurn = Input.GetAxis("Mouse X") * MouseSensetivity * Time.deltaTime;
+        transform.Rotate(0, horizontalTurn, 0);
+    }
+}
